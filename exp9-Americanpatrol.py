f0c99a2bfbef66b15d44f99e1bd78b7c129be98a
Haneef4128 <114045201+Haneef4128@users.noreply.github.com>
2023-08-22 09:53:40 +0530

    Americanpatrol

--- exp9-Americanpatrol.py
+++ exp9-Americanpatrol.py
@@ -1,49 +1,40 @@
-def prepare_input(text):
-    text = text.replace(" ", "").upper()
-    return text
-
-def find_char_position(matrix, char):
-    for row in range(5):
-        for col in range(5):
-            if matrix[row][col] == char:
-                return row, col
-    return -1, -1
-
-def playfair_decrypt(ciphertext, key):
-    decrypted_text = ""
-    index = 0
-
-    while index < len(ciphertext):
-        char1 = ciphertext[index]
-        char2 = ciphertext[index + 1]
-        index += 2
-
-        row1, col1 = find_char_position(key, char1)
-        row2, col2 = find_char_position(key, char2)
-
-        if row1 != -1 and row2 != -1:
-            if row1 == row2:
-                decrypted_text += key[row1][(col1 - 1) % 5] + key[row2][(col2 - 1) % 5]
-            elif col1 == col2:
-                decrypted_text += key[(row1 - 1) % 5][col1] + key[(row2 - 1) % 5][col2]
-            else:
-                decrypted_text += key[row1][col2] + key[row2][col1]
+def playfair_decrypt(key, message):
+    key = key.replace(" ", "").upper()  # Remove spaces and convert to uppercase
+    key_matrix = [[key[i + j*5] for i in range(5)] for j in range(5)]
+    
+    message = message.replace(" ", "").upper()
+    decrypted = ""
+    i = 0
+    
+    while i < len(message):
+        char1, char2 = message[i], message[i+1]
+        
+        row1, col1 = None, None
+        row2, col2 = None, None
+        
+        for r in range(5):
+            if char1 in key_matrix[r]:
+                row1, col1 = r, key_matrix[r].index(char1)
+            if char2 in key_matrix[r]:
+                row2, col2 = r, key_matrix[r].index(char2)
+        
+        # Handle cases where characters are not found in the key matrix
+        if row1 is None or col1 is None or row2 is None or col2 is None:
+            decrypted += " "
         else:
-            decrypted_text += char1 + char2
+            if row1 == row2:
+                decrypted += key_matrix[row1][(col1 - 1) % 5] + key_matrix[row2][(col2 - 1) % 5]
+            elif col1 == col2:
+                decrypted += key_matrix[(row1 - 1) % 5][col1] + key_matrix[(row2 - 1) % 5][col2]
+            else:
+                decrypted += key_matrix[row1][col2] + key_matrix[row2][col1]
+        
+        i += 2
+    
+    return decrypted
 
-    return decrypted_text
-
-key = [
-    ['K', 'X', 'J', 'E', 'Y'],
-    ['U', 'R', 'E', 'B', 'Z'],
-    ['W', 'H', 'T', 'F', 'S'],
-    ['C', 'G', 'A', 'M', 'D'],
-    ['P', 'Q', 'V', 'I', 'N']
-]
-
-ciphertext = "KXJEYUREBEZWEHEWRYTUHEYFSKREHEGOYFIWTTTUOLKSYCAJPOBOTEIZONTXBYBNTGONEYCUZWRGDSONSXBOUYWRHEBAAHYUSEDQ"
-
-ciphertext = prepare_input(ciphertext)
-decrypted_text = playfair_decrypt(ciphertext, key)
-
-print("Decrypted:", decrypted_text)
+key = 'K E Y F S T U X J H B N O P C A G V W R I L D Q Z W M'
+message = 'KXJEY UREBE ZWEHE WRYTU HEYFS KREHE GOYFI WTTTU OLKSY CAJPO BOTEI ZONTX BYBNT GONEY CUZWR GDSON SXBOU YWRHE BAAHY USEDQ'
+print("Message:",message)
+decrypted = playfair_decrypt(key, message)
+print("\nDecrypt:",decrypted)
